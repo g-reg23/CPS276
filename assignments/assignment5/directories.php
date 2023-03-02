2bf43cdc4bf60624df5af57ea0a7d8f39c5b4ff0
g-reg23 <gztauf5420@gmail.com/>
2023-03-02 01:22:42 -0500

    updated

--- assignments/assignment5/directories.php
+++ assignments/assignment5/directories.php
@@ -1,17 +1,19 @@
 <?php 
     class Directories {
         function createDirAndFile() {
-            $old = umask(0);
+            // $old = umask(0);
             $dir = "directories/".$_POST['folderName'];
             echo "**Folder Name**".$dir;
             if ( !file_exists($dir) ) {
                 $success = mkdir($dir);
-                // file_put_contents($dir.'/readme.txt', $_POST['readmetext']);
+                if ($success) {
+                    file_put_contents($dir.'/readme.txt', $_POST['readmetext']);
+                }
                 return $dir.'/readme.txt';
             } else {
                 echo "Folder exists";
             }
-            umask($old);
+            // umask($old);
         }
     }
 ?>
